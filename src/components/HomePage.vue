990b6efcea5dcf1584ac0948bc461979f414318b
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2022-09-07 22:37:43 -0400

    Setup initial state for the project
    Unnecessary assets where deleted and components where renamed:
    - remove vue logo
    - remove unnecessary styles and homepage content

---
+++ src/components/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>CFTP Server Statistics</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+</style>
